771b847e1105e8104e8d1a0b2ecbe3b4bbe1e133
Devansh Raghav <Devanshraghavsn@gmail.com>
2025-09-28 19:16:15 +0530

    build again for cors

--- dist/src/app.js
+++ dist/src/app.js
@@ -15,20 +15,20 @@ const adminRoutes_1 = __importDefault(require("./routes/adminRoutes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(cookieParser());
-console.log({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// console.log({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 app.use((0, helmet_1.default)());
-app.use((0, cors_1.default)({ origin: ["http://localhost:3000"], credentials: true }));
+app.use((0, cors_1.default)({ origin: ["http://localhost:3000", "https://dev-fintrade-app.netlify.app"], credentials: true }));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes_1.default);
 app.use("/api/products", productRoutes_1.default);
 app.use("/api/transactions", transactionRoutes_1.default);
 app.use("/api/admin", adminRoutes_1.default);
-app.get("/", (res) => {
-    res.send("Financial Trading App API is running");
-});
+// app.get("/", (res:Response) => {
+//   res.send("Financial Trading App API is running");
+// });
 exports.default = app;
